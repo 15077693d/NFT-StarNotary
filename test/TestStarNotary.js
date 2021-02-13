6db7cd12a1fbf901ac2138e72e9a888e3add3e1d
15077693d <15077693d@gmail.com>
2021-02-13 23:30:19 +0800

    change assert

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -15,7 +15,7 @@ it("Can create a star", async () => {
     await instance.createStar(starName, tokenId, {
             from:accounts[0]
         })
-    assert(await instance.tokenIdToInfo.call(tokenId),starName)
+    assert.equal(await instance.tokenIdToInfo.call(tokenId),starName)
 })
 
 it("lets user1 put up thier star for sale", async () => {
@@ -25,8 +25,8 @@ it("lets user1 put up thier star for sale", async () => {
     await instance.createStar(starName, tokenId,{
         from:accounts[0]
     })
-    await instance.putStarToSale(tokenId, 100,{from:accounts[0]});
-    assert(await instance.tokenIdToPrice,100)
+    await instance.putStarToSale(tokenId, 1,{from:accounts[0]});
+    assert.equal(await instance.tokenIdToPrice.call(tokenId),1)
 })
 
 it('lets user1 get the funds after the sale', async () => {
@@ -36,23 +36,23 @@ it('lets user1 get the funds after the sale', async () => {
     let instance = await StarNotary.deployed();
     let seller = accounts[0]
     let buyer = accounts[1]
-    let sellerBalance1 = await web3.eth.getBalance(seller)
     await instance.createStar(starName, tokenId,{
         from: seller
     })
     let owner =  await instance.ownerOf(tokenId)
-    assert(owner,seller)
+    assert.equal(owner,seller)
     await instance.putStarToSale(tokenId,price,{
         from: seller
     })
+    let sellerBalance1 = await web3.eth.getBalance(seller)
     await instance.buyStar(tokenId,{
         from: buyer,
         value:price
     })
     owner =  await instance.ownerOf(tokenId)
-    assert(owner,buyer)
+    assert.equal(owner,buyer)
     let sellerBalance2 = Number(sellerBalance1) + Number(price)
-    assert(sellerBalance2, await web3.eth.getBalance(seller))
+    assert.equal(sellerBalance2, await web3.eth.getBalance(seller))
 })
 
 it('lets user2 buy a star, if it is put up for sale', async() => {
@@ -66,7 +66,7 @@ it('lets user2 buy a star, if it is put up for sale', async() => {
     await instance.putStarToSale(starId, starPrice, {from: user1});
     let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
     await instance.buyStar(starId, {from: user2, value: balance});
-    assert(await instance.ownerOf(starId), user2);
+    assert.equal(await instance.ownerOf(starId), user2);
 });
 
 it('lets user2 buy a star and decreases its balance in ether', async() => {
@@ -83,13 +83,13 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
     await instance.buyStar(starId, {from: user2, value: balance, gasPrice:0});
     const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
     let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
-    assert(value, starPrice);
+    assert.equal(value, starPrice);
   });
 
   it('can add the star name and star symbol properly', async() => {
     let instance  = await StarNotary.deployed();
-    assert("StarNotary", await instance.name())  
-    assert("STAR",await instance.symbol())  
+    assert.equal("StarNotary", await instance.name())  
+    assert.equal("STAR",await instance.symbol())  
   });
 
   it('lets 2 users exchange stars', async() => {
@@ -98,11 +98,11 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
       let user2 = accounts[1]
       await instance.createStar("star1", 6,{ from:user1})
       await instance.createStar("star2", 7,{ from:user2})
-      assert(user1, await instance.ownerOf(6));
-      assert(user2, await instance.ownerOf(7));
+      assert.equal(user1, await instance.ownerOf(6));
+      assert.equal(user2, await instance.ownerOf(7));
       await instance.exchangeStars(6,user1,7,user2,{ from:user1})
-      assert(user1, await instance.ownerOf(7));
-      assert(user2, await instance.ownerOf(6));
+      assert.equal(user1, await instance.ownerOf(7));
+      assert.equal(user2, await instance.ownerOf(6));
   })
 
   it('lets a user transfer a star',async() => {
@@ -110,7 +110,7 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
       let owner1 = accounts[0]
       let owner2 = accounts[1]
       await instance.createStar("star1", 8,{from:owner1})
-      assert(owner1, await instance.ownerOf(8))
+      assert.equal(owner1, await instance.ownerOf(8))
       await instance.transferStar(owner2, 8,{from:owner1})
-      assert(owner2, await instance.ownerOf(8))
+      assert.equal(owner2, await instance.ownerOf(8))
   })
